0b3576922c2b7f112c8556780492075f1570020f
ShrutiKathiriya-coder <kathiriyashruti3@gmail.com>
2024-11-27 18:52:09 +0530

    Add files via upload

---
+++ project1.js
@@ -0,0 +1,66 @@
+// 1) Wrtie a Javascript program to find the area of a traingle. 
+
+let h = 40, l = 57;
+let tra = (h * l) / 2;
+
+console.log("1) Area Of Traingle = " + tra);
+
+ 
+
+
+
+// 2) Wrtie a Javascript program to convert temperature to and from celsius, farenheit.
+
+let cel = 47;
+let fah = (cel * 9) / 5 + 32;
+
+console.log("2) " + cel + " °C = " + fah + " °F");
+
+
+
+// 3) Wrtie a Javascript program to find the area of rectangle. 
+
+let leng = 50, bre = 80;
+let rectangleArea = leng * bre;
+
+console.log("3) Area Of Rectangle = " + rectangleArea);
+
+
+
+// 4) Wrtie a Javascript program to find the area of circle.
+
+let r = 7;
+let circleArea = 3.14 * r * r;
+
+console.log("4) Area Of Circle = " + circleArea);
+
+
+
+// 5) Wrtie a Javascript program to convert feet into inches.
+
+let feet = 5;
+let inch = feet * 62;
+
+console.log("2) " + feet + " feet = " + inch + " inch");
+
+
+
+// 6) Wrtie a Javascript program to calculate given formula :-
+// i) (a^2) - (b^2) = (a-b)(a+b)
+
+let a = 7, b = 2;
+let ans=(a-b)*(a+b);
+console.log("(a-b)(a+b)" +ans)///ans=45
+
+// 2)answer
+
+let c=4,d=6;
+let anss=(c+d)*2;
+console.log("c*c-2cd+d*d :"+anss); ///anss-20
+
+//ans 3
+
+let e=5,f=6,g=7;
+let answer=(e+f+g)*(e+f+g);
+console.log(answer);///ans=324
+
